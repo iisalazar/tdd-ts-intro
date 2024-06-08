3a419f673c258db321db65db72ce88d99109b9ae
iisalazar <iansalazar67@gmail.com>
2024-06-08 21:03:06 +0800

    init commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+class Application {
+    constructor() {
+        console.log("Application created");
+    }
+
+    run() {
+        console.log("Application running");
+    }
+}
+
+export default Application;
